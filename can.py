587eb165ee2763fcbb76ec0f714e15f627b16031
XiaochangHu <xiaochanghu94@163.com>
2020-03-16 12:19:16 +0800

    Add files via upload

--- can.py
+++ can.py
@@ -2,12 +2,14 @@
 from scipy.spatial.distance import cdist
 import scipy.sparse.linalg as sparse
 from sklearn.cluster import KMeans
+import scipy as sp
 import  numpy as np
 import time
 import faiss
-from faiss import normalize_L2
 import torch
 import matplotlib.pyplot  as plt
+import scipy.io as scio
+import fastGNMF
 
 class Can():
     def __init__(self,k,m,num_class):
@@ -22,36 +24,29 @@ class Can():
         res = faiss.StandardGpuResources()
         index_flat = faiss.IndexFlatL2(d)
         gpu_index_flat = faiss.index_cpu_to_gpu(res, 0, index_flat)
-        X = X.astype('float32')
-        normalize_L2(X)
         gpu_index_flat.add(X)
         N = X.shape[0]
-        Nidx = gpu_index_flat.ntotal
-        print('There are %d samples' % Nidx);
-
         c = time.time()
         self.D, self.I = gpu_index_flat.search(X, self.m)
         elapsed = time.time() - c
-        print('kNN Search done in %d seconds' % elapsed)
 
         rr = np.zeros(N);
         A = np.zeros((N, N));
         for i in range(N):
             di = np.array(self.D[i, 1:self.k + 2])
-
+            if(i==0):
+                print(di)
             rr[i] = 0.5 * (self.k * di[self.k] - sum(di[0:self.k]))
             id = self.I[i, 1:self.k + 2];
-            c = (di[self.k] - di) / (self.k * di[self.k] - sum(di[1:self.k]) + 0.000000001);
-            A[i, id] = (di[self.k] - di) / (self.k * di[self.k] - sum(di[1:self.k]) + 0.000000001);
+            A[i, id] = (di[self.k] - di) / (self.k * di[self.k] - sum(di[1:self.k]) + 2.2204e-16);
         self.r = sum(rr) / len(rr);
         self.Lambda = self.r;
         A0 = (A + A.transpose()) / 2;
         D0 = np.eye(N) * A0.sum(axis=1);
         L0 = D0 - A0;
-        plt.imshow(L0*255)
-        plt.axis('off')
-        plt.show()
-        return L0
+
+
+        return L0,A0
 
 
     def max_0(self, x):
@@ -105,7 +100,8 @@ class Can():
 
     def graph_update(self, X_s, X_f, iter):
         # X_s n*d; X_f n*d
-        distf = cdist(X_f, X_f, metric='euclidean')
+        done = False;
+        distf = cdist(X_f, X_f, metric='sqeuclidean')
         N = X_s.shape[0]
         N1 = X_f.shape[0]
         if N != N1:
@@ -115,44 +111,45 @@ class Can():
             res = faiss.StandardGpuResources()
             index_flat = faiss.IndexFlatL2(d)
             gpu_index_flat = faiss.index_cpu_to_gpu(res, 0, index_flat)
-
-            normalize_L2(X_s)
             gpu_index_flat.add(X_s)
-
-            Nidx = gpu_index_flat.ntotal
-            print('There are %d samples' % Nidx);
-
             c = time.time()
             self.D, self.I = gpu_index_flat.search(X_s, self.m)
             elapsed = time.time() - c
             print('kNN Search done in %d seconds' % elapsed)
-
         A = np.zeros((N, N));
         for i in range(N):
-            idxa0 = self.I[i, 2:self.k + 2];
+            idxa0 = self.I[i, 1:self.k + 1];
             dfi = distf[i, idxa0];
-            dxi = self.D[i, idxa0];
+            dxi = self.D[i, 1:self.k + 1];
             ad = -(dxi + self.Lambda * dfi) / (2 * self.r);
             A[i, idxa0] = self.EProjSimplex_new(ad, 1);
+
+        print('epoch_{}'.format(iter))
+
         A0 = (A + A.transpose()) / 2;
         D0 = np.eye(N) * A0.sum(axis=1);
         L0 = D0 - A0;
-        vals, vecs = sparse.eigs(L0, k=self.num_class + 1)
-        fn1 = sum(vals[0:self.num_class]);
-        fn2 = sum(vals[0:self.num_class]);
-        if fn1 > 0.000000001:
+
+        vals, vecs = sparse.eigs(L0, 20, tol=0.01, which="SR")
+        b = np.array(np.array(sorted(enumerate(vals.real), key=lambda x: x[1]))[:,0]).astype('int32')
+
+        fn1 = sum(vals.real[b[0:self.num_class]]);
+        fn2 = sum(vals.real[b[0:self.num_class+1]]);
+        F_old = X_f;
+        F = vecs.real[:,b[0:self.num_class]]
+        print(vals.real[b[0:10]])
+        if fn1 > 0.0001: #set value must > 0.00001 debug by yexiaocheng
             self.Lambda = self.Lambda * 2;
-        elif fn2 < 0.00000000001:
-            self.Lambda = self.Lambda / 2;
-            return 1;
-        vecs[0:self.num_class]
-        X = vecs.real
-        rows_norm = np.linalg.norm(X, axis=1, ord=2)
-        Y = (X.T / rows_norm).T
-        kmeans = KMeans(n_clusters=self.num_class, random_state=1231)
-        Y = kmeans.fit(Y).labels_;
-        print(len(Y));
-        return Y, L0, A0
+        elif fn2 < 0.0001: #set value must > 0.00001 debug by yexiaocheng
+            self.Lambda = self.Lambda / 2; F = F_old;
+            print("F = F_old")
+        else:
+            print('get it')
+            done = True;
+
+        kmeans = KMeans(n_clusters=self.num_class)
+        Y = kmeans.fit(F).labels_;
+        return done,Y, L0, A0 , F
 
     def label_progation(self):
         # label pro
@@ -171,6 +168,16 @@ class Can():
         # D = np.array(1. / np.sqrt(S))
         # D = scipy.sparse.diags(D.reshape(-1))
         # Wn = D * W * D
+
+        #spetcal cluster
+        #
+        # degreeMatrix = np.sum(A, axis=1)
+        # sqrtDegreeMatrix = np.diag(1.0 / (degreeMatrix ** (0.5)))
+        # DWD = np.dot(np.dot(sqrtDegreeMatrix, A), sqrtDegreeMatrix)
+        #
+        # vals, vecs = sparse.eigs(DWD,k=2)
+
+
         return 0
 
 def dbmoon(N=100, d=2, r=10, w=2):
@@ -194,9 +201,7 @@ def dbmoon(N=100, d=2, r=10, w=2):
             data = np.concatenate((data, tmp.take(idx, axis=0)), axis=0)
         if data.shape[0] >= N:
             done = False
-    print(data)
     db_moon = data[0:N, :]
-    print(db_moon)
     # generate double moon data ----down
     data_t = np.empty([N, 2])
     data_t[:, 0] = data[0:N, 0] + r
@@ -205,13 +210,35 @@ def dbmoon(N=100, d=2, r=10, w=2):
     return db_moon
 
 if __name__=='__main__':
-    N = 200
-    d = -2
-    r = 10
-    w = 2
-    a = 0.1
-    data = dbmoon(N, d, r, w)
-    plt.plot(data[0:N, 0], data[0:N, 1], 'r*', data[N:2 * N, 0], data[N:2 * N, 1], 'b*')
-    plt.show()
-    Graph = Can(k=15,m=50,num_class=2)
-    L = Graph.graph_init(data)
+
+    # data = np.ascontiguousarray(scio.loadmat('dat100.mat')['ppp'],dtype='float32');
+    # Graph = Can(k=15,m=20,num_class=2)
+    # L, A = Graph.graph_init(data);
+    # vals, vecs = sparse.eigs(L, k=2, tol=0.001, which="SR")
+    # VECS = vecs.real
+    #
+    # for iter in range(50):
+    #     done,Y, L0, A0 , F = Graph.graph_update(data,VECS,iter);
+    #     plt.cla();
+    #     index1 = [i for i, x in enumerate(Y) if x == 0]
+    #     index2 = [i for i, x in enumerate(Y) if x == 1]
+    #     plt.title('{}_epoch'.format(iter))
+    #     plt.plot(data[index1, 0], data[index1, 1], 'r*')
+    #     plt.plot(data[index2, 0], data[index2, 1], 'b*')
+    #     plt.pause(0.1)
+    #     if done:
+    #         break;
+    #     VECS = F;
+    # plt.show()
+
+    # groundtruth ~ to obtain the cluster labels
+    X, groundtruth = fastGNMF.examples.COIL20.read_dataset(rank=10, image_num=5)
+
+    # initialize gnmf instance with rank=10 and p=5 for p-nearest neighbors
+    #  use default parameters for the rest (lambda = 0.5)
+    gnmf = fastGNMF.Gnmf(X=X, rank=10, p=4)
+    U, V = gnmf.factorize()
+
+    # output a t-sne image
+    fastGNMF.examples.COIL20.visualize_tsne(V, 10, groundtruth, "COIL20 test with rank=10; lambda=0.5; p=4", "test.png",
+                                            tsne_perplexity=5)
